2d895ceec2316438dd393ab827591797e5e732fb
Radetic <danielradetic@gmail.com>
2023-09-09 20:38:46 -0300

    route 21 level matching route 1 and including fishing mon in water encounters

--- data/wild/maps/Route21.asm
+++ data/wild/maps/Route21.asm
@@ -1,4 +1,29 @@
 Route21Mons:
+IF DEF(_RADZ) || DEF(LNKN) || DEF(_GLAU)
+	db 25 ; grass encounter rate
+	db 2, RATTATA
+	db 2, PIDGEY
+	db 2, RATTATA
+	db 2, PIDGEY
+	db 4, TANGELA
+	db 5, TANGELA
+	db 5, TANGELA
+	db 5, BULBASAUR
+	db 5, CHARMANDER
+	db 5, SQUIRTLE
+
+	db 5 ; water encounter rate
+	db 2, TENTACOOL
+	db 2, TENTACOOL
+	db 4, TENTACOOL
+	db 5, TENTACOOL
+	db 5, TENTACOOL
+	db 2, GOLDEEN
+	db 2, POLIWAG
+	db 3, STARYU
+	db 3, HORSEA
+	db 3, SHELLDER
+ELSE
 	db 25 ; grass encounter rate
 	db 21, RATTATA
 	db 23, PIDGEY
@@ -22,3 +47,4 @@ Route21Mons:
 	db 30, TENTACOOL
 	db 35, TENTACOOL
 	db 40, TENTACOOL
+ENDC
