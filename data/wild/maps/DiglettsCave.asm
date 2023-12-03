2d1bf7b3e01fca06f0b1f98b84514b0219c62dde
Radetic <danielradetic@gmail.com>
2023-12-03 16:51:36 -0300

    matching diglett's cave to route 3

--- data/wild/maps/DiglettsCave.asm
+++ data/wild/maps/DiglettsCave.asm
@@ -1,16 +1,16 @@
 CaveMons:
 	db 20 ; grass encounter rate
 IF DEF(_RADZ) || DEF(LNKN) || DEF(_GLAU)
-	db 3, DIGLETT
-	db 3, DIGLETT
+	db 4, DIGLETT
+	db 4, DIGLETT
 	db 3, DIGLETT
 	db 4, DIGLETT
 	db 3, DIGLETT
 	db 3, DIGLETT
-	db 4, DIGLETT
-	db 4, DIGLETT
-	db 6, DIGLETT
-	db 6, DIGLETT
+	db 5, DIGLETT
+	db 5, DIGLETT
+	db 7, DIGLETT
+	db 8, DIGLETT
 ELSE
 	db 18, DIGLETT
 	db 19, DIGLETT
