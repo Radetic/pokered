17c64cd2001ff88bf34b6e047adba6f86f1c75df
Radetic <danielradetic@gmail.com>
2023-12-18 16:50:40 -0300

    getting back green without extras

--- data/sgb/sgb_border.asm
+++ data/sgb/sgb_border.asm
@@ -1,25 +1,25 @@
 BorderPalettes:
-IF DEF(_RADZ) || DEF(_GREEN)
+IF DEF(_GREEN)
 	INCBIN "gfx/sgb/green_border.tilemap"
-ELIF DEF(_LNKN) || DEF(_RED)
+ELIF DEF(_RED)
 	INCBIN "gfx/sgb/red_border.tilemap"
-ELIF DEF(_GLAU) || DEF(_BLUE)
+ELIF DEF(_BLUE)
 	INCBIN "gfx/sgb/blue_border.tilemap"
 ENDC
 
 	ds $100
 
-IF DEF(_RADZ) || DEF(_GREEN)
+IF DEF(_GREEN)
 	RGB 30,29,29 ; PAL_SGB1
 	RGB 11,16,08
 	RGB 16,23,12
 	RGB 22,31,16
-ELIF DEF(_LNKN) || DEF(_RED)
+ELIF DEF(_RED)
 	RGB 30,29,29 ; PAL_SGB1
 	RGB 25,22,25
 	RGB 25,17,21
 	RGB 24,14,12
-ELIF DEF(_GLAU) || DEF(_BLUE)
+ELIF DEF(_BLUE)
 	RGB 0,0,0 ; PAL_SGB1 (the first color is not defined, but if used, turns up as 30,29,29... o_O)
 	RGB 10,17,26
 	RGB 5,9,20
@@ -28,17 +28,17 @@ ENDC
 
 	ds $18
 
-IF DEF(_RADZ) || DEF(_GREEN)
+IF DEF(_GREEN)
 	RGB 30,29,29 ; PAL_SGB2
 	RGB 22,31,16
 	RGB 27,20,6
 	RGB 15,15,15
-ELIF DEF(_LNKN) || DEF(_RED)
+ELIF DEF(_RED)
 	RGB 30,29,29 ; PAL_SGB2
 	RGB 22,31,16
 	RGB 27,20,6
 	RGB 15,15,15
-ELIF DEF(_GLAU) || DEF(_BLUE)
+ELIF DEF(_BLUE)
 	RGB 30,29,29 ; PAL_SGB2
 	RGB 27,11,6
 	RGB 5,9,20
@@ -47,17 +47,17 @@ ENDC
 
 	ds $18
 
-IF DEF(_RADZ) || DEF(_GREEN)
+IF DEF(_GREEN)
 	RGB 30,29,29 ; PAL_SGB3
 	RGB 31,31,17
 	RGB 18,21,29
 	RGB 15,15,15
-ELIF DEF(_LNKN) || DEF(_RED)
+ELIF DEF(_RED)
 	RGB 30,29,29 ; PAL_SGB3
 	RGB 31,31,17
 	RGB 18,21,29
 	RGB 15,15,15
-ELIF DEF(_GLAU) || DEF(_BLUE)
+ELIF DEF(_BLUE)
 	RGB 30,29,29 ; PAL_SGB3
 	RGB 12,15,11
 	RGB 5,9,20
@@ -67,10 +67,10 @@ ENDC
 	ds $18
 
 SGBBorderGraphics:
-IF DEF(_RADZ) || DEF(_GREEN)
+IF DEF(_GREEN)
 	INCBIN "gfx/sgb/green_border.2bpp"
-ELIF DEF(_LNKN) || DEF(_RED)
+ELIF DEF(_RED)
 	INCBIN "gfx/sgb/red_border.2bpp"
-ELIF DEF(_GLAU) || DEF(_BLUE)
+ELIF DEF(_BLUE)
 	INCBIN "gfx/sgb/blue_border.2bpp"
 ENDC
