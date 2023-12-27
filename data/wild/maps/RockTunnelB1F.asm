ae85bb1f5d448a6eed30fc9dd45c41794bfd4c06
Radetic <danielradetic@gmail.com>
2023-12-27 10:16:13 -0300

    regression of wild encounters for rock tunnel

--- data/wild/maps/RockTunnelB1F.asm
+++ data/wild/maps/RockTunnelB1F.asm
@@ -1,5 +1,17 @@
 TunnelMonsB2:
 	db 15 ; grass encounter rate
+IF DEF(_ROAM)
+	db 3, ZUBAT
+	db 4, ZUBAT
+	db 4, GEODUDE
+	db 3, MACHOP
+	db 3, GEODUDE
+	db 5, ZUBAT
+	db 4, MACHOP
+	db 4, ONIX
+	db 2, ONIX
+	db 5, GEODUDE
+ELSE
 	db 16, ZUBAT
 	db 17, ZUBAT
 	db 17, GEODUDE
@@ -10,5 +22,6 @@ TunnelMonsB2:
 	db 17, ONIX
 	db 13, ONIX
 	db 18, GEODUDE
+ENDC
 
 	db 0 ; water encounter rate
